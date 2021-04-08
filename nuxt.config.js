a5a3fd105de12720c2522b70fe036a136b18130e
Alex Collier <alexkcollier@gmail.com>
2021-04-08 17:59:53 -0400

    chore: lints file

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,18 +6,18 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ]
+      { hid: 'description', name: 'description', content: pkg.description },
+    ],
   },
   css: ['~/assets/styles/fonts.scss'],
   loading: false,
   build: {
     loaders: {
       scss: {
-        implementation: require('sass')
-      }
+        implementation: require('sass'),
+      },
     },
-    extend(config, ctx) {
+    extend (config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
@@ -26,10 +26,10 @@ module.exports = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/,
           options: {
-            fix: true
-          }
+            fix: true,
+          },
         })
       }
-    }
-  }
+    },
+  },
 }
