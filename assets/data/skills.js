837b62dcf21867aa2bed608d99dbd2a6e53db70f
Alex Collier <alexkcollier@gmail.com>
2021-04-09 09:23:29 -0400

    feat: adds `SkillList` component

---
+++ assets/data/skills.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    group: 'Design',
+    list: [
+      'UX design',
+      'Wireframing',
+      'Prototyping',
+      'Iconography',
+      'Accessibility',
+      'Web design',
+    ],
+  },
+  {
+    group: 'Tools',
+    list: [
+      'Adobe XD',
+      'Framer',
+      'Photoshop',
+      'Illustrator',
+      'InDesign',
+      'Premiere Pro',
+    ],
+  },
+  {
+    group: 'Code',
+    list: [
+      'HTML/CSS',
+      'JavaScript',
+      'TypeScript',
+      'Vue.js',
+      'React',
+      'Git',
+    ],
+  },
+  {
+    group: 'Other',
+    list: [
+      'Flexible',
+      'Quick learner',
+      'Detail oriented',
+      'Resourceful',
+      'Passionate',
+      'Marketing',
+    ],
+  },
+];
---
+++ components/SkillList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="skill-list">
+    <strong
+      v-if="title"
+      class="skill-list__title"
+    >
+      {{ title }}
+    </strong>
+
+    <ul class="skill-list__list">
+      <li
+        v-for="skill in skillList"
+        :key="skill"
+        class="skill-list__item"
+      >
+        {{ skill }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkillList',
+
+  props: {
+    skillList: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.skill-list {
+  margin-bottom: 1rem;
+
+  &__title {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  &__list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
