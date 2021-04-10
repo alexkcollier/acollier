fca9a39cb9a97e31689ac6098ab698e427d2773f
Alex Collier <alex.collier@spartanbio.com>
2021-04-10 12:20:47 -0400

    feat: adds sidebar links to additional projects

--- pages/work/_slug.vue
+++ pages/work/_slug.vue
@@ -16,6 +16,30 @@
 
           <ToolsList :tools="content.tools" />
         </section>
+
+        <section
+          v-if="links && links.length"
+          class="sidebar__section work-links"
+        >
+          <h2 class="heading-1">
+            More projects
+          </h2>
+
+          <ul class="work-links__list">
+            <li
+              v-for="({ path, title }) in links"
+              :key="path"
+              class="work-links__list-item"
+            >
+              <NuxtLink
+                class="link link--arrow"
+                :to="path"
+              >
+                {{ title }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </section>
       </aside>
     </div>
   </div>
@@ -31,9 +55,20 @@ export default {
 
   async asyncData ({ $content, route }) {
     const content = await $content(route.path).fetch();
+    const links = await $content('work')
+      .only([
+        'path',
+        'title',
+      ])
+      .where({ path: { $ne: route.path } })
+      .limit(5)
+      .fetch()
+      // swallow errors
+      .catch(() => { });
 
     return {
       content,
+      links,
     };
   },
 
@@ -56,4 +91,20 @@ export default {
 
 <style lang="scss">
 @use '~/assets/styles/nuxt-content';
+@use '~/assets/styles/utils/mixins';
+
+.work-links {
+  position: sticky;
+  top: 0.5rem;
+
+  &__list {
+    @include mixins.unstyled-list;
+  }
+
+  &__list-item {
+    &:not(:last-child) {
+      margin-bottom: 1.5rem;
+    }
+  }
+}
 </style>
