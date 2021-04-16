2bf52c1ac9da4b8fe6d093a55c0e60c1b2a40076
alexkcollier <alexkcollier@gmail.com>
2021-04-16 12:45:45 -0400

    feat: adds error logging

--- functions/mail.js
+++ functions/mail.js
@@ -2,49 +2,101 @@ const FormData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(FormData);
 const mg = mailgun.client({ username: 'api', key: process.env.MG_API_KEY || 'key-yourkeyhere' });
+const Sentry = require('@sentry/node');
 
-exports.handler = async (event) => {
-  try {
-    // only accept POST
-    if (event.httpMethod !== 'POST') {
-      throw new Error('Invalid request method');
-    }
+const response500 = {
+  statusCode: 500,
+  body: 'Something went wrong',
+};
+let sentryInitialized = false;
 
-    // verify request has body, filter honeypot
-    const data = JSON.parse(event.body);
+/**
+ * Initializes sentry
+ */
+function initSentry () {
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
+  sentryInitialized = true;
+}
 
-    if (!data || data.honeypot !== '') {
-      throw new Error('Invalid');
-    }
+/**
+ * Reports error correctly
+ * @param {Error|string} err Error
+ * @returns {Promise<boolean>|void}
+ */
+async function reportError (err) {
+  if (process.env.NODE_ENV !== 'production') {
+    return console.log(err);
+  }
 
-    // send mail
-    const { message } = await mg.messages.create(process.env.MG_DOMAIN, {
-      from: `acollier.ca <noreply@${process.env.MG_DOMAIN}>`,
-      to: process.env.MG_RECIPIENT,
-      subject: `New contact form submission from ${process.env.MG_DOMAIN}`,
-      text: `
+  if (!sentryInitialized) {
+    return;
+  }
+
+  typeof err === 'string' ? Sentry.captureMessage(err) : Sentry.captureException(err);
+  return Sentry.flush();
+}
+
+/**
+ * Send email
+ * @param {{ name: string, email: string, message: string }} data message data
+ * @returns {Promise<string>} response message
+ */
+async function sendMail (data) {
+  const body = `
 ===
 Name: ${data.name}
 Email: ${data.email}
 ===
 
 ${data.message}
-`,
-    });
+`;
 
-    // update client
+  const { message } = await mg.messages.create(process.env.MG_DOMAIN, {
+    from: `acollier.ca <noreply@${process.env.MG_DOMAIN}>`,
+    to: process.env.MG_RECIPIENT,
+    subject: `New contact form submission from ${process.env.MG_DOMAIN}`,
+    text: body,
+  });
+
+  return message;
+}
+
+initSentry();
+
+/**
+ * Netlify Function handler
+ * @param event Netlify Function event
+ * @param context Netlify Function context
+ * @returns {Promise<{ statusCode: Number, body: string }>}
+ */
+exports.handler = async (event, context) => {
+  // Make sure AWS doesn't wait for an empty event loop, as that can break things with Sentry
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  // only accept POST
+  if (event.httpMethod !== 'POST') {
+    return response500;
+  }
+
+  // verify request has body, filter honeypot
+  const data = JSON.parse(event.body);
+
+  if (!data || data.honeypot !== '') {
+    return response500;
+  }
+
+  try {
+    // Send mail
+    const message = await sendMail(data);
+
     return {
       statusCode: 200,
       body: message,
     };
   } catch (err) {
-    if (process.env.NODE_ENV !== 'production') {
-      console.log(err);
-    };
+    // Send errors to Sentry
+    await reportError(err);
 
-    return {
-      statusCode: 500,
-      body: err.message || 'Something went wrong',
-    };
+    return response500;
   }
 };
