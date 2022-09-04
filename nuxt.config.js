a2d9fe1c8ea8cd01bf726dae5fe514fa90624705
Alex Collier <alexkcollier@gmail.com>
2022-09-04 18:02:26 -0400

    chore: updates nonbreaking dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,22 +32,6 @@ module.exports = {
     '~/assets/styles/styles.scss',
   ],
   loading: false,
-  build: {
-    extend (config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-          options: {
-            fix: true,
-          },
-        });
-      }
-    },
-  },
   plugins: [
     '~/plugins/vue-lazyload.js',
   ],
